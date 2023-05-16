5e63792af64e313fa82486b2c351865c389e249a
yongggnd <yongkerkwith@gmail.com>
2023-05-16 14:58:09 +0700

    user struct return json format

--- playground.go
+++ playground.go
@@ -1,20 +1,32 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"encoding/json"
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type User struct {
+	ID   int
+	Name string
+	Age  int
+}
 
 func main() {
+	users := []User{
+		{ID: 1, Name: "Test", Age: 10},
+	}
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello world")
+		return c.SendString("Hello " + strconv.Itoa(users[0].ID) + " " + users[0].Name)
 	})
 
-	app.Get("/:value", func(c *fiber.Ctx) error {
-		return c.SendString("receive parameter " + c.Params("value"))
-	})
-
-	app.Get("/more/*", func(c *fiber.Ctx) error {
-		return c.SendString("more parameters " + c.Params("*"))
+	app.Get("/user", func(c *fiber.Ctx) error {
+		returnUsers, _ := json.Marshal(users)
+		return c.SendString(string(returnUsers))
 	})
 
 	app.Listen(":8000")
