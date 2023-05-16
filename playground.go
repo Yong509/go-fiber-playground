20ea39fb682bdb222130dd41e9451a9a4d68b684
yongggnd <yongkerkwith@gmail.com>
2023-05-16 15:08:00 +0700

    format json

--- playground.go
+++ playground.go
@@ -1,22 +1,28 @@
 package main
 
 import (
-	"encoding/json"
 	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 type User struct {
-	ID   int
-	Name string
-	Age  int
+	ID   int    `json:"ID"`
+	Name string `json:"Name"`
+	Age  int    `json:"Age"`
+}
+
+var users = []User{
+	{ID: 1, Name: "James", Age: 10},
+	{ID: 2, Name: "George", Age: 20},
+	{ID: 3, Name: "John", Age: 30},
+}
+
+func getUser(c *fiber.Ctx) error {
+	return c.JSON(users)
 }
 
 func main() {
-	users := []User{
-		{ID: 1, Name: "Test", Age: 10},
-	}
 
 	app := fiber.New()
 
@@ -24,10 +30,7 @@ func main() {
 		return c.SendString("Hello " + strconv.Itoa(users[0].ID) + " " + users[0].Name)
 	})
 
-	app.Get("/user", func(c *fiber.Ctx) error {
-		returnUsers, _ := json.Marshal(users)
-		return c.SendString(string(returnUsers))
-	})
+	app.Get("/user", getUser)
 
 	app.Listen(":8000")
 }
